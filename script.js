e2ab9e06a97c9790c74e1e6808189cd31e75759a
Grace <frankgracey@gmail.com>
2021-08-28 21:00:51 +0100

    added getQuotes function for fetching quotes from API

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Get Quotes From API
+let quotes = [];
+
 async function getQuotes() {
   const URL = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(URL);
+    quotes = await response.json();
+  } catch (err) {
+    console.log(err);
+    quotes = localQuotes;
+  }
 }
+
+function newQuote() {
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+//Onload
+getQuotes();
