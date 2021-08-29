1b988cb2413718461837a9ce7a050328b91779ed
Grace <frankgracey@gmail.com>
2021-08-29 08:37:42 +0100

    tweet button an  new quote button now working

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-// Get Quotes From API
 let quotes = [];
 
+// Selectors
+const authorText = document.getElementById("author");
+const quoteText = document.getElementById("quote");
+const tweetButton = document.getElementById("tweet-button");
+const newQuoteButton = document.getElementById("new-quote-button");
+
+// Get Quotes From API
 async function getQuotes() {
   const URL = "https://type.fit/api/quotes";
 
   try {
     const response = await fetch(URL);
     quotes = await response.json();
+    newQuote();
   } catch (err) {
     console.log(err);
     quotes = localQuotes;
   }
 }
 
+//generate new random quote
 function newQuote() {
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  authorText.textContent = !quote.author ? "Unknown" : quote.author;
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteButton.addEventListener("click", newQuote);
+tweetButton.addEventListener("click", tweetQuote);
 
 //Onload
 getQuotes();
